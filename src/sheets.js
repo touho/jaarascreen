e2aa7be948cae1adf350dd72426fc70513b8d015
Marko Rintamäki <marko.rintamaki@gmail.com>
2018-07-25 12:56:40 +0300

    no program offset. jaarascreen is ready for rumble!

--- src/sheets.js
+++ src/sheets.js
@@ -93,7 +93,8 @@ async function updateCache() {
 						info: cells[i + 8].value
 					};
 
-					entry.timestamp = new Date(entry.timestamp.getTime() - 1000 * 60 * 60 * 24 * 4);
+					// move all program by days:
+					//entry.timestamp = new Date(entry.timestamp.getTime() - 1000 * 60 * 60 * 24 * 0);
 
 					entry.endTime = new Date(entry.timestamp.getTime() + 1000*60*entry.durationMinutes);
 
